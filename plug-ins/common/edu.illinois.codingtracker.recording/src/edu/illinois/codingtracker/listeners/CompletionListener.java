9c2405b437d192c67a8d55600142bf3c8759dfc8
Joffre Yagual <joffre.yc@gmail.com>
2014-04-23 21:34:24 -0300

    It relates to issue karlitrotski/CodingSpectator/#4. Added listener for quick-fix invocation via error icon. Programmed by @joffreyc85

---
+++ plug-ins/common/edu.illinois.codingtracker.recording/src/edu/illinois/codingtracker/listeners/CompletionListener.java
@@ -0,0 +1,98 @@
+/**
+ * This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
+ */
+package edu.illinois.codingtracker.listeners;
+
+import org.eclipse.jface.text.ITextOperationTarget;
+import org.eclipse.jface.text.contentassist.ContentAssistEvent;
+import org.eclipse.jface.text.contentassist.ICompletionListener;
+import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.jface.text.source.ISourceViewerExtension3;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+
+
+/**
+ * 
+ * @author Joffre Yagual
+ * 
+ */
+//@SuppressWarnings("restriction")
+public class CompletionListener extends BasicListener implements ICompletionListener {
+
+	private static CompletionListener instance = null;
+
+	public static CompletionListener getInstance() {
+		if (instance == null) {
+			instance = new CompletionListener();
+		}
+
+		return instance;
+	}
+
+	private CompletionListener() {
+		super();
+	}
+
+	public static void register()
+	{
+		
+		Display.getDefault().asyncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				boolean isCompletionListenerRegistered= false;
+				while (!isCompletionListenerRegistered) {
+					IWorkbenchWindow activeWindow= getActiveWorkbenchWindow();
+					
+					if (activeWindow != null) {
+						IWorkbenchPage activePage= activeWindow.getActivePage();
+						IEditorPart editor = activePage.getActiveEditor();
+						ISourceViewerExtension3 sourceViewerExtension3 = 
+								CompletionListener.getSourceViewerExtension3(editor);
+						if (sourceViewerExtension3 != null) {
+							if (sourceViewerExtension3.getQuickAssistAssistant() != null) {
+								sourceViewerExtension3.getQuickAssistAssistant()
+										.addCompletionListener(CompletionListener.getInstance());
+							}
+						isCompletionListenerRegistered= true;
+					}
+				}
+			}
+			}
+		});
+
+	}
+	
+	public static ISourceViewerExtension3 getSourceViewerExtension3(
+			IEditorPart editor) {
+		if (editor == null) {
+			return null;
+		}
+
+		ISourceViewer viewer = (ISourceViewer) editor
+				.getAdapter(ITextOperationTarget.class);
+		if (viewer instanceof ISourceViewerExtension3) {
+			return (ISourceViewerExtension3) viewer;
+		}
+
+		return null;
+	}
+		
+	public void assistSessionStarted(ContentAssistEvent event) {
+		//Write code here for processing the event
+	}
+
+	public void assistSessionEnded(ContentAssistEvent event) {
+
+	}
+
+	public void selectionChanged(ICompletionProposal proposal,
+			boolean smartToggle) {
+
+	}
+
+}
