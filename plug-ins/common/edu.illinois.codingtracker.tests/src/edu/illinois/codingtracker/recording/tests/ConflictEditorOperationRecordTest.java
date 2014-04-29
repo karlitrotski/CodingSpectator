defb1db3863be5151439e44dfecd1cde31723347
Matias Pardo G <mat.pardo@gmail.com>
2014-04-28 21:17:11 -0400

    Moving Recording Tests.
    Deleting project edu.illinois.codingtracker.recording.tests and put a new package for this purpose in edu.illinois.codingtracker.tests.
    It relates to issue karlitrotski/CodingSpectator/29.

--- plug-ins/common/edu.illinois.codingtracker.tests/src/edu/illinois/codingtracker/recording/tests/ConflictEditorOperationRecordTest.java
+++ plug-ins/common/edu.illinois.codingtracker.tests/src/edu/illinois/codingtracker/recording/tests/ConflictEditorOperationRecordTest.java
@@ -5,12 +5,20 @@ package edu.illinois.codingtracker.recording.tests;
 
 import static org.junit.Assert.*;
 
+import edu.illinois.codingtracker.operations.conflicteditors.*;
 import org.junit.*;
 /**
  * @author Matías Pardo Gutiérrez
  *
  */
 public class ConflictEditorOperationRecordTest {
+	
+	private OpenedConflictEditorOperation a = new OpenedConflictEditorOperation();
+	
+	@Before
+	public void setUp(){
+		
+	}
 	//Test for ConflictEditorOperation
 	//Test for OpenedConflictEditorOperation
 	//Test for SavedConflictEditorOperation
