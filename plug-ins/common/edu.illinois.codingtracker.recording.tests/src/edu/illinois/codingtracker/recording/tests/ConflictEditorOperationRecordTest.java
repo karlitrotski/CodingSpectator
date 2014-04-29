ea8b5bffb0338210943313e9354371e9a1725166
Matias Pardo G <mat.pardo@gmail.com>
2014-04-28 20:14:21 -0400

    Creation of Project for Record Testing
    Creation of project edu.illinois.codingtracker.recording.tests.
    It relates to issue https://github.com/karlitrotski/CodingSpectator/issues/29.
    Starting programming with @dmunozl and @ccsorip

---
+++ plug-ins/common/edu.illinois.codingtracker.recording.tests/src/edu/illinois/codingtracker/recording/tests/ConflictEditorOperationRecordTest.java
@@ -0,0 +1,17 @@
+/**
+ * This file is licensed under the University of Illinois/NCSA Open Source License. See LICENSE.TXT for details.
+ */
+package edu.illinois.codingtracker.recording.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+/**
+ * @author Matías Pardo Gutiérrez
+ *
+ */
+public class ConflictEditorOperationRecordTest {
+	//Test for ConflictEditorOperation
+	//Test for OpenedConflictEditorOperation
+	//Test for SavedConflictEditorOperation
+}
