9c2c0e438916af8bb269f73cb8e1d14eb5d31a58
karlitrotski <cpdettoni@gmail.com>
2014-05-29 01:23:16 -0400

    Function run() added to MonitorTask class. It writes the changes on plug-ins states, using the states detected in the last time interval. Related to issue karlitrotski/CodingSpectator/#13 Pair programming with @sesanchez

--- plug-ins/common/edu.illinois.codingtracker.operations/src/edu/illinois/codingtracker/operations/pluginusage/MonitorTask.java
+++ plug-ins/common/edu.illinois.codingtracker.operations/src/edu/illinois/codingtracker/operations/pluginusage/MonitorTask.java
@@ -16,6 +16,7 @@ import edu.illinois.codingtracker.operations.starts.PluginsList;
 public class MonitorTask extends TimerTask {
 	
 	TreeMap<String, Integer> bundlesMap = new TreeMap<String, Integer>(String.CASE_INSENSITIVE_ORDER);
+	public StringBuffer text = new StringBuffer("");
 	
 	public MonitorTask(){
 		IBundleGroupProvider[] providers = Platform.getBundleGroupProviders();
@@ -35,7 +36,26 @@ public class MonitorTask extends TimerTask {
 	
 	@Override
 	public void run() {
-		
+		int numstate;
+		IBundleGroupProvider[] providers = Platform.getBundleGroupProviders();
+		if (providers != null) {
+		    for (int i = 0; i < providers.length; i++) {
+		        IBundleGroup[] bundleGroups = providers[i].getBundleGroups();
+		        for (int j = 0; j < bundleGroups.length; j++) {
+		            Bundle[] bundles = bundleGroups[j] == null ? new Bundle[0] : bundleGroups[j]
+		                    .getBundles();
+		            for (int k = 0; k < bundles.length; k++) {
+		            	numstate = bundles[k].getState();
+		            	if(bundlesMap.get(bundles[k].getSymbolicName()) == null){
+		            		text.append("New plug-in: "+bundles[k].getSymbolicName()+" - State: "+StateSymbols.getStateName(bundles[k].getState())+"\n");
+		            	}
+		            	else if(bundlesMap.get(bundles[k].getSymbolicName()) != numstate){
+		            		text.append("Plug-in "+bundles[k].getSymbolicName()+" changed state from "+StateSymbols.getStateName(bundlesMap.get(bundles[k].getSymbolicName()))+" to "+StateSymbols.getStateName(numstate)+"\n");
+		            	}
+		            }
+		        }
+		    }		    		    
+		}
 	}
 
 }
