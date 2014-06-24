7c5af2b6ccc19d216ec8000fb9209fffdb16177a
Juraj Kubelka <Juraj.Kubelka@gmail.com>
2014-06-24 18:00:35 -0400

    New test case OperationSymbolsTest
    It has detected duplication in OperationSymbols.
    Related to issue karlitrotski/CodingSpectator/#35.
    Pair programming with @cinai.

---
+++ plug-ins/common/edu.illinois.codingtracker.tests/src/edu/illinois/codingtracker/tests/OperationSymbolsTest.java
@@ -0,0 +1,54 @@
+package edu.illinois.codingtracker.tests;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import edu.illinois.codingtracker.operations.OperationDeserializer;
+import edu.illinois.codingtracker.operations.OperationSymbols;
+import edu.illinois.codingtracker.operations.UserOperation;
+
+public class OperationSymbolsTest {
+
+	@Test
+	public void testNotEmpty() {
+		ArrayList<Character> symbols = getSymbolValues();
+		assertTrue(symbols.size() > 0);
+	}
+
+	@Test
+	public void testNotDuplicatedSymbols() {
+		ArrayList<Character> symbols = getSymbolValues();
+		ArrayList<Character> checked = new ArrayList<Character>();
+		for (Character symbol: symbols) {
+			if (checked.contains(symbol))
+				fail("Duplicated symbol " + symbol);
+			else
+				checked.add(symbol);
+		}
+	}
+
+	public ArrayList<Character> getSymbolValues() {
+		ArrayList<Character> values = new ArrayList<Character>();
+		Field[] fields = OperationSymbols.class.getDeclaredFields();
+		for (Field field: fields) {
+			Class<?> type = field.getType();
+			if (type == char.class) {
+				char value;
+				try {
+					value = field.getChar(OperationSymbols.class);
+					values.add(value);
+				} catch (IllegalArgumentException e) {
+					fail("Fail getting field " + field.toString() + ", error: " + e.toString());
+				} catch (IllegalAccessException e) {
+					fail("Fail getting field " + field.toString() + ", error: " + e.toString());
+				}				
+			}
+		}
+		return values;
+	}
+
+}
