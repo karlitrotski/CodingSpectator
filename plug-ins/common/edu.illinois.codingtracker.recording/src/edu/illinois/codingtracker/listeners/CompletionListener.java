b65ede7cb769e5fb7b9b4aa4d28def0fd6f2108c
Maite González <maite_G1@hotmail.com>
2014-06-30 14:49:36 -0400

    Listener  QuickfixUsage(CompletionListener)
    Listener that hears the usage of any quickfix
    Related to issue #4
    Programmed by @joffreyc85
    Colaborate: @maitegm

--- plug-ins/common/edu.illinois.codingtracker.recording/src/edu/illinois/codingtracker/listeners/CompletionListener.java
+++ plug-ins/common/edu.illinois.codingtracker.recording/src/edu/illinois/codingtracker/listeners/CompletionListener.java
@@ -3,16 +3,49 @@
  */
 package edu.illinois.codingtracker.listeners;
 
+import javax.sound.sampled.Control;
+
+import org.eclipse.jdt.internal.ui.text.correction.IStatusLineProposal;
+import org.eclipse.jdt.internal.ui.text.correction.JavaCorrectionAssistant;
+import org.eclipse.jdt.internal.ui.text.correction.JavaCorrectionProcessor;
+import org.eclipse.jdt.ui.text.java.IQuickAssistProcessor;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextHover;
+import org.eclipse.jface.text.ITextHoverExtension2;
+import org.eclipse.jface.text.DefaultTextHover;
 import org.eclipse.jface.text.ITextOperationTarget;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.ITextViewerExtension2;
+import org.eclipse.jface.text.ITextHoverExtension2;
 import org.eclipse.jface.text.contentassist.ContentAssistEvent;
+import org.eclipse.jface.text.contentassist.ContentAssistant;
 import org.eclipse.jface.text.contentassist.ICompletionListener;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
+import org.eclipse.jface.text.contentassist.ICompletionProposalExtension3;
+import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
+import org.eclipse.jface.text.contentassist.IContentAssistant;
+import org.eclipse.jface.text.contentassist.IContentAssistantExtension2;
+import org.eclipse.jface.text.quickassist.QuickAssistAssistant;
+import org.eclipse.jface.text.source.ContentAssistantFacade;
 import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.jface.text.source.ISourceViewerExtension2;
 import org.eclipse.jface.text.source.ISourceViewerExtension3;
+import org.eclipse.jface.text.source.ISourceViewerExtension4;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
+import org.eclipse.ui.texteditor.ITextEditor;
+import org.eclipse.jface.text.contentassist.IContentAssistantExtension;
+import org.eclipse.swt.events.*;
 
 
 /**
@@ -44,24 +77,19 @@ public class CompletionListener extends BasicListener implements ICompletionList
 
 			@Override
 			public void run() {
-				boolean isCompletionListenerRegistered= false;
-				while (!isCompletionListenerRegistered) {
-					IWorkbenchWindow activeWindow= getActiveWorkbenchWindow();
-					
-					if (activeWindow != null) {
-						IWorkbenchPage activePage= activeWindow.getActivePage();
-						IEditorPart editor = activePage.getActiveEditor();
-						ISourceViewerExtension3 sourceViewerExtension3 = 
-								CompletionListener.getSourceViewerExtension3(editor);
-						if (sourceViewerExtension3 != null) {
-							if (sourceViewerExtension3.getQuickAssistAssistant() != null) {
-								sourceViewerExtension3.getQuickAssistAssistant()
-										.addCompletionListener(CompletionListener.getInstance());
-							}
-						isCompletionListenerRegistered= true;
+				IWorkbenchWindow activeWindow= getActiveWorkbenchWindow();	
+				if (activeWindow != null) {
+					IWorkbenchPage activePage= activeWindow.getActivePage();
+					IEditorPart editor = activePage.getActiveEditor();
+					ISourceViewerExtension3 sourceViewerExtension3 = 
+							CompletionListener.getSourceViewerExtension3(editor);
+					if (sourceViewerExtension3 != null) {
+						if (sourceViewerExtension3.getQuickAssistAssistant() != null) {
+							sourceViewerExtension3.getQuickAssistAssistant()
+									.addCompletionListener(CompletionListener.getInstance());
+						}
 					}
 				}
-			}
 			}
 		});
 
@@ -82,17 +110,80 @@ public class CompletionListener extends BasicListener implements ICompletionList
 		return null;
 	}
 		
-	public void assistSessionStarted(ContentAssistEvent event) {	
+		
+	@SuppressWarnings("deprecation")
+	public void assistSessionStarted(ContentAssistEvent event) {
+		//Write code here for processing the event
 		operationRecorder.recordNewCompletionQuickfix(event);
-	}
 
+		IWorkbenchWindow activeWindow= getActiveWorkbenchWindow();
+		IWorkbenchPage activePage= activeWindow.getActivePage();
+		IEditorPart editor = activePage.getActiveEditor();
+		ISourceViewerExtension3 viewer = CompletionListener.getSourceViewerExtension3(editor);
+		ISourceViewerExtension4 viewer0 = (ISourceViewerExtension4) viewer;
+		ITextViewer tviewer = (ITextViewer)viewer;
+		ITextViewerExtension2 tviewerext = (ITextViewerExtension2)viewer;
+		ITextHover defaultHover = new DefaultTextHover((ISourceViewer)viewer);
+		IRegion hr = defaultHover.getHoverRegion(tviewer, viewer.getQuickAssistInvocationContext().getOffset());
+		//ITextHoverExtension2 dhext = (ITextHoverExtension2)defaultHover;
+		String s = defaultHover.getHoverInfo(tviewer, hr);
+		if (tviewerext.getCurrentTextHover() != null) {
+			int j = 0;
+			j++;		
+			
+		}
+		//ITextHover ito = tviewerext.getCurrentTextHover();
+		String s1 = tviewer.getTextWidget().getSelectionText();
+		ICompletionProposal [] icp = viewer.getQuickAssistAssistant().getQuickAssistProcessor().computeQuickAssistProposals(viewer.getQuickAssistInvocationContext());
+		int offset = viewer.getQuickAssistInvocationContext().getOffset();
+		//tviewer.getTextWidget().getToolTipText();
+		if (viewer.getCurrentAnnotationHover() != null) {
+			//String texto = viewer.getCurrentAnnotationHover().getHoverInfo((ISourceViewer)viewer, viewer.getQuickAssistInvocationContext().getOffset());
+		}
+	}
+	
 	public void assistSessionEnded(ContentAssistEvent event) {
 		//operationRecorder.recordNewQuickfixUsage(event);
 	}
 
 	public void selectionChanged(ICompletionProposal proposal,
 			boolean smartToggle) {
-
+		IWorkbenchWindow activeWindow= getActiveWorkbenchWindow();
+		IWorkbenchPage activePage= activeWindow.getActivePage();
+		IEditorPart editor = activePage.getActiveEditor();
+		ISourceViewerExtension3 viewer = CompletionListener.getSourceViewerExtension3(editor);
+		ITextViewer tviewer = (ITextViewer)viewer;
+		Shell sh = tviewer.getTextWidget().getShell();
+		Shell [] shs = sh.getShells();
+		Table tb = null;
+		for (int i= 0; i< shs.length; i++) {
+			org.eclipse.swt.widgets.Control [] shChildren = shs[i].getChildren();
+			for (int j= 0; j< shChildren.length; j++) {
+				if (shChildren[j] instanceof Table) {
+					tb = (Table)shChildren[j];
+					break;
+				}
+			}
+		}
+		if (tb != null) {
+			org.eclipse.swt.events.SelectionListener listener2= new org.eclipse.swt.events.SelectionListener() {
+				private String itemText;
+				public void widgetSelected(SelectionEvent e) {
+					if(e.item instanceof TableItem) {
+						TableItem ti = (TableItem)e.item;
+						itemText = ti.getText();
+					}
+				}
+				public void widgetDefaultSelected(SelectionEvent e) {
+					String result = itemText;
+					System.out.println(result);
+					operationRecorder.recordNewQuickfixUsage(result);
+				}
+			};
+			
+			tb.addSelectionListener(listener2);
+		}
 	}
+
 
 }
